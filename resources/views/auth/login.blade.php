5eb75c506b6ea22e59fa240de64db4695129855e
Victor Gabriel <victorgabriel08fs@gmail.com>
2021-12-17 17:40:58 -0300

    Corrigindo objeto de retorno da Home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,11 +57,11 @@
                                     {{ __('Entrar') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Esqueceu sua senha?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>

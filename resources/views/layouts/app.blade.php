d75d30148a1d4c2dc3463ba4c864d5c81e149323
Victor Gabriel <victorgabriel08fs@gmail.com>
2021-12-27 15:16:26 -0300

    Subindo favicon para main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,11 +17,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-<<<<<<< HEAD
-=======
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="{{ asset('images/coin.png') }}">
->>>>>>> local
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -98,7 +95,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                 document.getElementById('logout-form').submit();">
+                                                                                                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 

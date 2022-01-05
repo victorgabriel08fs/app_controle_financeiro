e330c1bd69dfe3895eadaf2d3ce0ca5cb4f201ce
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-01-05 16:12:14 -0300

    Corrigindo

---
+++ resources/views/admin/conta/_components/formulario.blade.php
@@ -0,0 +1,47 @@
+@if (isset($conta->id))
+    <form action="{{ route('conta.update', ['conta' => $conta]) }}" method="POST">
+        @csrf
+        @method('PUT')
+    @else
+        <form action="{{ route('conta.store') }}" method="POST">
+            @csrf
+@endif
+<input type="hidden" name="user_id" value={{ $user_id }}>
+<div class="mb-3">
+    <label class="form-label">CEP</label>
+    <input type="text" class="form-control" name="cep" value="{{ $endereco->cep ?? old('cep') }}">
+    {{ $errors->has('cep') ? $errors->first('cep') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Rua</label>
+    <input type="text" class="form-control" name="rua" value="{{ $endereco->rua ?? old('rua') }}">
+    {{ $errors->has('rua') ? $errors->first('rua') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Número</label>
+    <input type="text" class="form-control" name="numero" value="{{ $endereco->numero ?? old('numero') }}">
+    {{ $errors->has('numero') ? $errors->first('numero') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Complemento</label>
+    <input type="text" class="form-control" name="complemento"
+        value="{{ $endereco->complemento ?? old('complemento') }}">
+    {{ $errors->has('complemento') ? $errors->first('complemento') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Bairro</label>
+    <input type="text" class="form-control" name="bairro" value="{{ $endereco->bairro ?? old('bairro') }}">
+    {{ $errors->has('bairro') ? $errors->first('bairro') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Cidade</label>
+    <input type="text" class="form-control" name="cidade" value="{{ $endereco->cidade ?? old('cidade') }}">
+    {{ $errors->has('cidade') ? $errors->first('cidade') : '' }}
+</div>
+<div class="mb-3">
+    <label class="form-label">Estado</label>
+    <input type="text" class="form-control" name="estado" value="{{ $endereco->estado ?? old('estado') }}">
+    {{ $errors->has('estado') ? $errors->first('estado') : '' }}
+</div>
+<button type="submit" class="btn btn-primary">Salvar</button>
+</form>
---
+++ resources/views/admin/conta/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Nova Conta</div>
+
+                    <div class="card-body">
+                        @component('admin.conta._components.formulario')
+
+                        @endcomponent
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

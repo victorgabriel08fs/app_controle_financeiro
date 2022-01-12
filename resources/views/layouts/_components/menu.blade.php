a18d3d1dc558ff2577a0a5ec7ee235c3f2e99b41
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-01-12 16:45:38 -0300

    Acesso à rota solicitações

--- resources/views/layouts/_components/menu.blade.php
+++ resources/views/layouts/_components/menu.blade.php
@@ -53,6 +53,9 @@
                                 <a class="dropdown-item" href="{{ route('taxa.index') }}">
                                     Taxas
                                 </a>
+                                <a class="dropdown-item" href="{{ route('solicitacao.index') }}">
+                                    Solicitações
+                                </a>
                             </div>
                         </li>
                     @endif
@@ -83,7 +86,7 @@
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
-                                                                                                                                         document.getElementById('logout-form').submit();">
+                                                                                                                                             document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
